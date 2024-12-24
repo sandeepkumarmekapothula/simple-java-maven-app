aa1663f595e93e43a129f1173881f168ed028f0d
sandeepkumarmekapothula <105371151+sandeepkumarmekapothula@users.noreply.github.com>
2024-12-24 17:56:44 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello Teja & Pradeep. Good Evening. Welcome to my Class";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
